819eff1a5e6c39d78a7c32663d1b27d08a554090
Bizilix <gleb.moz@mail.ru>
2023-05-25 20:08:39 +0300

    SaveNoteScreen.kt (SaveNotContent)

--- app/src/test/java/ui/components/SaveNoteScreen.kt
+++ app/src/test/java/ui/components/SaveNoteScreen.kt
@@ -195,6 +195,41 @@ private fun SaveNoteTopAppBar(
     }
 }
 
+@Composable
+private fun SaveNoteContent(
+    note:NoteModel,
+    onNoteChange: (NoteModel)->Unit
+){
+    Column(modifier = Modifier.fillMaxSize()) {
+        ContentTextField(label = "Title",
+            text =note.title,
+            onTextChange = { newTitle ->
+                onNoteChange.invoke(note.copy(title = newTitle))
+            }
+        )
+        ContentTextField(
+            modifier = Modifier
+                .heightIn(max = 240.dp)
+                .padding(top = 16.dp),
+            label = "Baby",
+            text = note.content,
+            onTextChange = { newContent ->
+                onNoteChange.invoke(note.copy(content = newContent))
+            }
+        )
+        val canBeCheckedOff: Boolean = note.isCheckedOff !=null
+        NotesCheckOption(isChecked = canBeCheckedOff,
+            onCheckedChange = { canBeCheckedOffNewValue ->
+                val isCheckedOff: Boolean? = if (canBeCheckedOffNewValue) false else null
+                onNoteChange.invoke(note.copy(isCheckedOff=isCheckedOff))
+            }
+        )
+
+
+        PickedColor(color = note.color)
+    }
+}
+
 
 
 
